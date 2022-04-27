e2cf8ed5d949b5783b77b2fb58045b607e2121d2
smidlaj <jozsef.smidla@ext.intland.com>
2022-04-27 13:18:09 +0200

    bomberman #1

---
+++ 12/bomberman/Camera.py
@@ -0,0 +1,78 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+import pyrr
+
+class Camera:
+	"""!
+		Kamera osztaly. A kamera mozgatasaert, beallitasaert felel.
+	"""
+	def __init__(self, x, y, z):
+		"""!
+			@param x: A kamera kezdo x koordinataja.
+			@param y: A kamera kezdo y koordinataja.
+			@param z: A kamera kezdo z koordinataja.
+		"""
+		self.x = x
+		self.y = y
+		self.z = z
+		self.dirX = 0
+		self.dirY = 0
+		self.dirZ = -1
+		self.angleVert = -90.0
+		self.angleHoriz = 0.0
+	
+	def move(self, dist):
+		"""!
+			Az kamera aktualis iranyanak megfelelo iranyba mozditja el a kamerat.
+			@param dist: Azt adja meg, hogy az iranyvektor hanyszorosaval mozduljunk el.
+		"""
+		self.x += self.dirX * dist
+		self.y += self.dirY * dist
+		self.z += self.dirZ * dist
+
+	def __update(self):
+		self.dirX = math.cos(math.radians(self.angleVert))
+		self.dirZ = math.sin(math.radians(self.angleVert))
+		self.dirY = math.sin(math.radians(self.angleHoriz))
+		length = math.sqrt(self.dirX ** 2 + self.dirY ** 2 + self.dirZ ** 2)
+		self.dirX /= length
+		self.dirY /= length
+		self.dirZ /= length
+
+	def rotateUpDown(self, movement):
+		"""!
+			A kamerat forgatja el az yz sik menten, az x tengely korul. Ugyel arra, hogy -45 és 45
+			fok kozott maradjunk, ne tudjunk 'hatrabukfencet' csinalni.
+		"""
+		self.angleHoriz += movement
+		self.angleHoriz = min(45.0, max(-45.0, self.angleHoriz))
+		self.__update()
+		pass
+
+	def rotateRightLeft(self, movement):
+		"""!
+			A kamerat forgatja el az xz sik menten, az y tengely korul.
+		"""
+		self.angleVert += movement
+		self.__update()
+		pass
+
+	def getMatrixForCubemap(self):
+		return pyrr.matrix44.create_look_at([0.0, 0.0, 0.0], 
+											[self.dirX, self.dirY, self.dirZ], 
+											[0.0, 1.0, 0.0])
+	def getMatrix(self):
+		return pyrr.matrix44.create_look_at([self.x, self.y, self.z], 
+											[self.x + self.dirX, self.y + self.dirY, self.z + self.dirZ], 
+											[0.0, 1.0, 0.0])
+
+	def apply(self):
+		"""!
+			Atadja az OpenGL-nek a kamera beallitasait.
+		"""
+		glLoadIdentity()
+		gluLookAt(self.x, self.y, self.z, 
+					self.x + self.dirX, 
+					self.y + self.dirY, 
+					self.z + self.dirZ, 0, 1, 0)
---
+++ 12/bomberman/vertex_shader_texture.vert
@@ -0,0 +1,29 @@
+#version 330
+layout(location = 0) in vec3 in_position;
+layout(location = 1) in vec3 in_normal;
+layout(location = 2) in vec2 in_texture;
+
+uniform mat4 projection;
+uniform mat4 world;
+uniform mat4 view;
+
+// szamitasokat sporolunk:
+uniform mat4 projViewWorld;
+uniform mat4 viewWorld;
+uniform mat4 normalMat;
+
+out vec3 v_position;
+out vec3 v_normal;
+out vec2 v_texture;
+
+void main() { 
+//   gl_Position = projection * view * world * vec4((in_position), 1.0);
+//   v_position = (view * world * vec4(in_position, 1.0)).xyz;
+
+   gl_Position = projection * viewWorld * vec4((in_position), 1.0);
+   v_position = (world * vec4(in_position, 1.0)).xyz;
+
+
+   v_normal = mat3(transpose(inverse( view * world ))) * in_normal; 
+   v_texture = in_texture; 
+}
